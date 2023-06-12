ea1ab0fb0067d200ee7bfebd5ac25eeb73096603
Jcmgc7 <Juancarmgc2017@gmail.com>
2023-06-12 10:02:10 +0200

    Este commit es para saber que estamos en la rama Bucles

--- src/Bucles/Impares.java
+++ src/Bucles/Impares.java
@@ -6,6 +6,7 @@ public class Impares {
 
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
+		System.out.println("Introduce un numero");
 		int numero = teclado.nextInt();
 		for(int i = numero; i >0; i--) {
 			if(i  %3 ==0) {
--- src/Bucles/Pedir_numero.java
+++ src/Bucles/Pedir_numero.java
@@ -6,7 +6,7 @@ public class Pedir_numero {
 
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
-		System.out.println("Ingrese un numero: ");
+		System.out.println("Ingrese un numero; ");
 		int numero = teclado.nextInt();
 		for(int i = numero; i >0; i--) {
 			System.out.println(i);
